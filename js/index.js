dd1e2879a4da029971479cb2a9c1c2469cf481c2
nurgle1 <space.pixies@gmail.com>
2017-05-17 14:04:23 -0400

    Made a few changes

---
+++ js/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    for(var x = 0; x < 16; x++) {
+        for(var y = 0; y < 16; y++) {
+            var unit = $("<div class='unit'></div>");
+            unit.appendTo('#container');
+        }
+    }
+});
+
+console.log("whatwhat");
